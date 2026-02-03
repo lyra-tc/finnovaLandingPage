e4a5202cd223c95c8e736de88d10a0a1d799d5d6
Ricardo0919 <rickisierra03@gmail.com>
2026-02-03 22:23:05 +0000

    Hotfix of forms component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,23 @@ import Hero from './components/Hero.vue';
 // import Features from './components/Features.vue'
 import Footer from './components/Footer.vue';
 import Investment from './components/Investment.vue';
+
+const setFavicon = () => {
+  const favicon = document.querySelector('link[rel="icon"]');
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  
+  if (isDark) {
+    favicon.href = '/favicon-dark.ico';
+  } else {
+    favicon.href = '/favicon-light.ico';
+  }
+};
+
+// Ejecutar al cargar
+setFavicon();
+
+// Escuchar cuando el usuario cambia el modo del sistema sin recargar la página
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setFavicon);
 </script>
 
 <template>
